85c451c50f57a97be9c361a394c19781589175b6
Nibha2405 <impervious.nibha@gmail.com>
2024-09-26 01:54:45 +0530

    First commit

--- src/Question/SplitEmailId.java
+++ src/Question/SplitEmailId.java
@@ -5,6 +5,8 @@ public class SplitEmailId {
 	public static void main(String[] args) {
 		
          System.out.println("My name");
-	}
+         System.out.println("Hello World");
+         System.out.println("Hello Family");
+ }
 
 }
